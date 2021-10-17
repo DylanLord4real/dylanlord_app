dae6292c290d8fadded85491dc4810c22d863d4a
Dylan Adekpe <mateurplord@gmail.com>
2021-10-17 16:48:36 +0000

    adding Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DetailAlbumComponent } from './components/detail-album/detail-album.component';
 import { FormsModule } from '@angular/forms';
+import { HttpErrorsInterceptor } from './Interceptors/http-errors.interceptors';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,11 @@ import { FormsModule } from '@angular/forms';
 
   ],
   providers: [
-
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true,
+    },
   ],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   BASE_URL_JAMENDO_AL: "https://api.jamendo.com/v3.0/artists/albums/",
-  BASE_URL_JAMENDO: "https://api.jamendo.com/v3.0/artists"
+  BASE_URL_JAMENDO_TR: "https://api.jamendo.com/v3.0/artists"
 };
